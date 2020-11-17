ada9a853a4c3e1ce9ccfe59daf9d771fa9475ee1
Brady Hoskins <bchoskins@Bradys-MacBook-Pro-2.local>
2020-11-16 18:32:52 -0600

    Created clean_phone and stub function

---
+++ clean_phone.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+def validate_phone(phone_number):
+    """ Ensure that phone numbers are in a valid format
+
+    Keyword arguments:
+    phone_number -- A Pandas Series of phone_numbers as strings
+    
+    """
+
+    return True
+
+print(validate_phone(True))
